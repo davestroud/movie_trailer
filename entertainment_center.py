7986b95e1e365e6bcec7f669e5a9aeda21ad433e
David Stroud <david@greymatter-ag.com>
2015-09-23 09:34:38 -0500

    format changes to conform to PEP8 style guidelines.  Used formatter in Pycharm

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,18 @@
 import fresh_tomatoes
-
-__author__ = 'davidstroud'
-
 import media
 
 god_father = media.Movie("The Godfather",
                          "A story of a mans live in la cosa nostra",
-                         "https://c2.staticflickr.com/4/3646/3701323796_585f6b1719.jpg",
-                         "https://youtu.be/sY1S34973zA")
+                         "https://c2.staticflickr.com/4/3646/3701323796_585f6b1719.jpg", "https://youtu.be/sY1S34973zA")
 casino = media.Movie("Casino",
                      "The story of the Las Vega Mafia",
-                     "http://static.rogerebert.com/uploads/movie/movie_poster/casino-1995/large_6rRqNMDjYdJhtIgYSwiX91QgBTq.jpg",
+                     "http://static.rogerebert.com/uploads/movie/movie_poster/casino-1995/large_6rRqNMDjYdJhtIgYSwiX91Q"
+                     "gBTq.jpg",
                      "https://www.youtube.com/watch?v=ZN6mp2NjMhs")
 the_departed = media.Movie("The Departed ",
                            "A rogue cop's undercover life in the Boston Mafia",
-                           "http://static.rogerebert.com/uploads/movie/movie_poster/the-departed-2007/large_tGLO9zw5ZtCeyyEWgbYGgsFxC6i.jpg",
+                           "http://static.rogerebert.com/uploads/movie/movie_poster/the-departed-2007/large_tGLO9zw5Zt"
+                           "CeyyEWgbYGgsFxC6i.jpg",
                            "https://www.youtube.com/watch?v=LfHZMMDGKJ0")
-movies = [god_father,casino,the_departed]
+movies = [god_father, casino, the_departed]
 fresh_tomatoes.open_movies_page(movies)
-
-
-
-
