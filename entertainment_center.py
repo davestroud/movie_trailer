cdfc8104aa9fd1bcfa62440a1862d8bc8ff49262
David Stroud <david@greymatter-ag.com>
2015-09-26 12:14:10 -0500

    add comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,6 @@
 import fresh_tomatoes
 import media
-
+# create  and define instances of the class movie to build the objects.
 god_father = media.Movie("The Godfather",
                          "A story of a mans live in la cosa nostra",
                          "https://c2.staticflickr.com/4/3646/3701323796_585f6b1719.jpg", "https://youtu.be/sY1S34973zA")
@@ -14,5 +14,7 @@ the_departed = media.Movie("The Departed ",
                            "http://static.rogerebert.com/uploads/movie/movie_poster/the-departed-2007/large_tGLO9zw5Zt"
                            "CeyyEWgbYGgsFxC6i.jpg",
                            "https://www.youtube.com/watch?v=LfHZMMDGKJ0")
+# define array of movies
 movies = [god_father, casino, the_departed]
+# calls the function open_movies_page
 fresh_tomatoes.open_movies_page(movies)
