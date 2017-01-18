364df16e451f7afedc01c0723594f736bd9e5318
David Stroud <david@greymatter-ag.com>
2017-01-18 11:54:18 -0500

    add new images with url shortner

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,16 +3,16 @@ import media
 # create  and define instances of the class movie to build the objects.
 # used url shortener to shorten youtube links
 god_father = media.Movie("The Godfather",
-                         "A story of a mans live in la cosa nostra",
-                         "https://goo.gl/Ad6SvC",
+                         "A story of a mans life in la cosa nostra",
+                         "https://goo.gl/2PMXIH",
                          "https://youtu.be/sY1S34973zA")
 casino = media.Movie("Casino",
                      "The story of the Las Vega Mafia",
-                     "http://goo.gl/QJnBoz",
+                     "https://goo.gl/043Mbq",
                      "https://youtu.be/-L5Zx34mjUU")
 the_departed = media.Movie("The Departed ",
                            "A rogue cop's undercover life in the Boston Mafia",
-                           "http://goo.gl/ClJt3o",
+                           "https://goo.gl/G2ALDY",
                            "https://youtu.be/iQpb1LoeVUc")
 # define array of movies
 movies = [god_father, casino, the_departed]
